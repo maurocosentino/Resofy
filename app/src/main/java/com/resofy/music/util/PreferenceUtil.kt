e6e964bdd32ac4e8b689e3f9b13f7f2ab855b57b
maurocosentino <mauroxcosentino@gmail.com>
2026-06-05 22:24:32 -0300

    fix(settings): set expanded appbar mode as default

--- app/src/main/java/com/resofy/music/util/PreferenceUtil.kt
+++ app/src/main/java/com/resofy/music/util/PreferenceUtil.kt
@@ -834,7 +834,7 @@ object PreferenceUtil {
         set(value) = sharedPreferences.edit { putFloat(PLAYBACK_PITCH, value) }
 
     val appBarMode: TopAppBarLayout.AppBarMode
-        get() = if (sharedPreferences.getString(APPBAR_MODE, "1") == "0") {
+        get() = if (sharedPreferences.getString(APPBAR_MODE, "0") == "0") {
             TopAppBarLayout.AppBarMode.COLLAPSING
         } else {
             TopAppBarLayout.AppBarMode.SIMPLE
